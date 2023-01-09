851bca42cae7fb5ea1784aa72459bfdc714996b1
achille <achillegley@gmail.com>
2023-01-09 16:50:16 +0700

    adding frontend files to Github pages

--- deployfrontend.cmd
+++ deployfrontend.cmd
@@ -1,5 +1,5 @@
-robocopy -r src docs /E
-robocopy -r build\contracts docs
+robocopy  src docs /E
+robocopy  build\contracts docs
 git add .
 git commit -m "adding frontend files to Github pages"
 git push
